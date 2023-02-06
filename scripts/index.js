6ba13c641623a13a8d767c42b27bcb6ab76f4bcc
Alex Ryabich <alexryabich@mail.ru>
2023-02-06 21:09:20 +0300

    новый проект

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const popup = document.querySelector('.popup');
+const OpenEditButton = document.querySelector('.profile__button');
+const closeButton = document.querySelector('.popup__button-close');
+let formElement = document.querySelector('.popup__container');
+let nameProfyleInput = formElement.querySelector('.popup__input_type_name');
+let jobProfyleInput = formElement.querySelector('.popup__input_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+popupClose();
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  nameProfyleInput.value = profileName.textContent;
+  jobProfyleInput.value = profileJob.textContent;
+}
+
+function popupClose() {
+    popup.classList.remove('popup_opened');  
+  }
+
+  function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameProfyleInput.value;
+    profileJob.textContent = jobProfyleInput.value;
+    popupClose();
+  }
+
+  closeButton.addEventListener('click', popupClose);
+  OpenEditButton.addEventListener('click', popupOpen);
+  formElement.addEventListener('submit', handleFormSubmit);
